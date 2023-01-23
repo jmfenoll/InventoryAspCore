c16927dbfed0abb41c7f70fb1e47f3a1e3ed7ebc
Juanma Fenoll <juanmanuelfenoll@MichaelPage.es>
2023-01-23 11:57:41 +0100

    Initial push

---
+++ InventoryAspCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InventoryAspCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ InventoryAspCore/Models/ProductViewModel.cs
@@ -0,0 +1,12 @@
+namespace InventoryAspCore.Models
+{
+    public class ProductViewModel
+    {
+        public string Id { get; set; }
+
+        public string Description { get; set; }
+
+        public int Quantity { get; set; }
+
+    }
+}
